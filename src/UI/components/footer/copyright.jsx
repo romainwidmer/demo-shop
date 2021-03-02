538cb010bb0e8f33721336f42e629156bdaaa88b
Romain Widmer <romwidmer@gmail.com>
2021-03-02 10:45:31 +0100

    footer

---
+++ src/UI/components/footer/copyright.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+const CopyrightComponent = () => (
+    <div className="copyright">
+        <p>&copy; 2021 romain widmer | Tout droits réservés</p>
+    </div>
+)
+
+export default CopyrightComponent
--- src/UI/components/footer/index.jsx
+++ src/UI/components/footer/index.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 
 const Footer = () => (
     <div className="main-footer">
-        <h1>Footer</h1>
+        <div className="container">
+            <section>
+                <img src="/img/logo.png" alt="logo-weekend" className="logo" />
+                <p className="negative-color">
+                    Romain Widmer
+                </p>
+                <p className="negative-color">
+                    Route de la Petite-Caroline 1A <br />
+                    1131 Tolochenaz <br />
+                    +41 78 725 35 25 <br />
+                    <a href="mailto:info@romwidmer@gmail.com">romwidmer@gmail.com</a>
+                </p>
+                <p className="nogativ-color">
+                    <a href="https://github.com/romainwidmer" target="_blank" rel="noopener noreferrer">https://github.com/romainwidmer</a>
+                </p>
+            </section>
+        </div>
+        <SkillListComponent />
+        <CopyrightComponent />
     </div>
 )
 
