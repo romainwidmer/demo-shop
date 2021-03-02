2a4bb212612c7ceb95e52ffe1e53014602f6c7e0
Romain Widmer <romwidmer@gmail.com>
2021-03-02 10:40:48 +0100

    cart context

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import ReactDOM from 'react-dom'
 // Import Router
 import AppRouter from './router'
 
+// Import Contexts
+import CartContextProvider from './contexts/cart'
+
+// Import libs style
+
+// Import personal styles
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>,
-  document.getElementById('root')
+  	<React.StrictMode>
+    	<CartContextProvider>
+      		<AppRouter />
+    	</CartContextProvider>
+  	</React.StrictMode>,
+	document.getElementById('root')
 )
